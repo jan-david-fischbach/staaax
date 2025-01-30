14a0f34b8a685aa128b01e9a488b4788eb2a8481
Jan David Fischbach <mail@jan-dav.id>
2025-01-30 10:06:53 +0100

    Add sign to tildify

--- staaax/tildify.py
+++ staaax/tildify.py
@@ -1,10 +1,26 @@
 from staaax.propagation import angled_sqrt
 import jax.numpy as jnp
 
-def tildify(k, Cs, bcs, nan_tolerance=0):
-    return 1/len(Cs)*jnp.sum(jnp.array([
+def tildify(k, Cs, bcs, nan_tolerance=0, sign=1, normalize=False):
+    """ Transform to mitigate influence of square root type branch cuts. 
+
+    Args:
+        k (complex): coordinate in untransformed space
+        Cs (List[complex]): Branchpoints
+        bcs (List[float]): Direction of the branchcuts
+        nan_tolerance (int, optional): In this vicinity (in radian) to the branchcut the squareroot wil return nan. Defaults to 0.
+        sign (int, optional): Used to flip the transform to allow for transforming kx instead of k0. Defaults to 1.
+
+    Returns:
+        complex: transformed coordinate 
+    """
+    if normalize:
+        norm = jnp.sum(jnp.array(Cs))
+    else:
+        norm = len(Cs)
+    return 1/norm*jnp.sum(jnp.array([
         angled_sqrt(
-            (k**2 - C**2), 
+            sign*(k**2 - C**2), 
             bc_angle=bc, 
             nan_tolerance=nan_tolerance) for C, bc in zip(Cs, bcs)
     ]), axis=0)
